e131865d4b8208b59f62a33c334d4880b3662978
creightontaylor <creightontaylor@gmail.com>
2014-11-25 15:33:41 -0500

    Initial Commit

---
+++ iClairApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iClairApp
+//
+//  Created by Creighton Taylor on 11/25/14.
+//  Copyright (c) 2014 iClair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
