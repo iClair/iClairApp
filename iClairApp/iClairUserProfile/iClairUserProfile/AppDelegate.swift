b385c42909dda455ebcd4078d27dbd43ca97e9c9
creightontaylor <creightontaylor@gmail.com>
2014-12-03 11:53:48 -0500

    "Added an MVP project for core functionality

--- iClairApp/iClairUserProfile/iClairUserProfile/AppDelegate.swift
+++ iClairApp/iClairUserProfile/iClairUserProfile/AppDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  AppDelegate.swift
-//  iClairApp
+//  iClairUserProfile
 //
-//  Created by Creighton Taylor on 11/25/14.
+//  Created by Creighton Taylor on 11/29/14.
 //  Copyright (c) 2014 iClair. All rights reserved.
 //
 
@@ -12,17 +12,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-	
+
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        
-        Parse.setApplicationId("eqosnm4gtVTVFaYmqCBbgWMj0tWwHCVCHfxa0cjw", clientKey: "IRZucAv2R5zyle62vtIMW3BFXWQLKmkbMqxU81qW")
-        PFFacebookUtils.initializeFacebook()
-        
-        FBLoginView.self
-        FBProfilePictureView.self
-        
         return true
     }
 
@@ -40,21 +33,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
     }
 
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-    func application(application: UIApplication,
-        openURL url: NSURL,
-        sourceApplication: String,
-        annotation: AnyObject?) -> Bool {
-            return FBAppCall.handleOpenURL(url, sourceApplication:sourceApplication,
-                withSession:PFFacebookUtils.session())
-    }
 
-    func applicationDidBecomeActive(application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        FBAppCall.handleDidBecomeActiveWithSession(PFFacebookUtils.session())
-    }
 }
 
---
+++ iClairApp/iClairUserProfile/iClairUserProfile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iClairUserProfile
+//
+//  Created by Creighton Taylor on 11/29/14.
+//  Copyright (c) 2014 iClair. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
